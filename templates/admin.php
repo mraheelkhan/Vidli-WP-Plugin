f4b9101539886c83ba89e0323d49b213ba8eac90
Raheel Khan <irahilkhan@gmail.com>
2021-03-27 00:52:30 +0500

    plugin settings page

--- templates/admin.php
+++ templates/admin.php
@@ -2,11 +2,34 @@
     <h1> Vidli Plugin Dashboard</h1>
     <?php settings_errors(); ?>
 
-    <form method="POST" action="options.php">
-        <?php
-            settings_fields('vidli_options_group');
-            do_settings_sections('vidli_plugin');
-            submit_button();
-        ?>
-    </form>
+
+    <ul class="nav nav-tabs">
+        <li class="active">
+            <a href="#tab-1">Manage Settings</a>
+        </li>
+        <li>
+            <a href="#tab-2">Updates</a>
+        </li>
+        <li>
+            <a href="#tab-3">About</a>
+        </li>
+    </ul>
+    <div class="tab-content">
+        <div id="tab-1" class="tab-pane active">
+            <form method="POST" action="options.php">
+                <?php
+                    settings_fields('vidli_options_group');
+                    do_settings_sections('vidli_plugin');
+                    submit_button();
+                ?>
+            </form>
+        </div>
+        <div id="tab-2" class="tab-pane">
+            Update
+        </div>
+        <div id="tab-3" class="tab-pane">
+            About
+        </div>
+    </div>
+    
 </div>
